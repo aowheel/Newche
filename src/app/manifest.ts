1eba78f0363e055b4f3a1772f85c1f387c60d713
aowheel <aoimakino2003@gmail.com>
2024-06-19 05:05:21 +0900

    Fine tuning

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'standalone',
     background_color: '#000000',
-    theme_color: '#ffffff',
+    theme_color: '#000000',
     icons: [
       {
         src: '/favicon.ico',
@@ -19,11 +19,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
